515d7c63051959e088143f38d626087f60ff9809
coderussell <joschka.kuznik@finlist.de>
2023-05-31 08:53:40 +0200

    minor formatting

--- src/helpers/fill.ts
+++ src/helpers/fill.ts
@@ -1,5 +1,3 @@
-import { grid } from "./grid";
-
 // fills in single cell returns updated input (sudoku)
 export default function fillCell(cellIndex: number, input: number[], solution: number): number[] {
   // fallback for invalid solutions
