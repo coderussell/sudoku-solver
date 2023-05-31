2864143350146cb33ca84306538566321c5c30f7
coderussell <joschka.kuznik@finlist.de>
2023-05-31 08:53:04 +0200

    added 'fillCell' function

---
+++ src/helpers/fill.ts
@@ -0,0 +1,12 @@
+import { grid } from "./grid";
+
+// fills in single cell returns updated input (sudoku)
+export default function fillCell(cellIndex: number, input: number[], solution: number): number[] {
+  // fallback for invalid solutions
+  if (!solution || !Number.isInteger(solution) || solution < 0 || solution > 9) return input;
+  const newInput = [...input]
+
+  newInput[cellIndex] = solution
+
+  return newInput;
+}
