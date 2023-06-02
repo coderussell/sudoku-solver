56e03a7e1a55c415df3272bf9a85664bd718da98
coderussell <joschka.kuznik@finlist.de>
2023-06-02 11:33:23 +0200

    fixed 'isComplete' for empty sections

--- src/helpers/complete.ts
+++ src/helpers/complete.ts
@@ -10,6 +10,8 @@ export interface CompleteSections {
 // checks if a row, column or square has each interger from 1 to 9 exactly once
 export default function isComplete(sectionType: 'rows' | 'columns' | 'squares', sectionIndex: number, input: number[]): boolean {
   let values: number[] = grid[sectionType][sectionIndex].map(i => input[i]).filter(v => v !== 0)
+  if (values.length === 0) return false;
+
   let uniqueValues: Set<number> = new Set([...values])
 
   const allCellsUniquleyFilled: boolean = uniqueValues.size === 9;
@@ -24,13 +26,17 @@ export function getCompleteSectionIndices(input: number[]): CompleteSections {
     let completeColumns: number[] = [];
     let completeSquares: number[] = [];
 
+
     for (let i = 0; i < 9; i++) {
-      if (isComplete('rows', i, input)) completeRows.push(i)
-      if (isComplete('columns', i, input)) completeColumns.push(i)
-      if (isComplete('squares', i, input)) completeSquares.push(i)
+      console.log(completeRows)
+      if (isComplete('rows', i, [...input])) completeRows.push(i)
+      if (isComplete('columns', i, [...input])) completeColumns.push(i)
+      if (isComplete('squares', i, [...input])) completeSquares.push(i)
     }
 
     const solved = completeRows.length === 9 && completeColumns.length === 9 && completeSquares.length === 9
+
+    console.log(solved)
 
     return {
       solved,
